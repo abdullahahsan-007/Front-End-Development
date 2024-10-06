d849ce96dfed47569722ec0e987dbab368df9ea3
@abdullahahsan <abdullahahsan337474@gmail.com>
2024-10-06 06:32:11 +0500

    Add files via upload

---
+++ ICT project/script.js
@@ -0,0 +1,28 @@
+    // script.js
+function goToNextPage() {
+  window.location.href = "page1.html";
+}
+  
+function iamback() {
+  window.location.href = "page2.html";
+
+}
+
+function starters() {
+  window.location.href = "page3.html";
+}
+
+function pizzadeals() {
+  window.location.href = "page4.html";
+}
+
+function pastas() {
+  window.location.href = "page5.html";
+}
+
+function burgerz() {
+  window.location.href = "page6.html";
+}
+function search1() {
+  window.location.href = "search.html";
+}
